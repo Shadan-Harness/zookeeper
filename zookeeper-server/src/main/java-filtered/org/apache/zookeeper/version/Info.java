4ef5d772e5708a8ada04615f77623a2aad102a23
Shadan-Harness <156399601+Shadan-Harness@users.noreply.github.com>
2024-01-26 21:39:35 +0530

    Update Info.java

--- zookeeper-server/src/main/java-filtered/org/apache/zookeeper/version/Info.java
+++ zookeeper-server/src/main/java-filtered/org/apache/zookeeper/version/Info.java
@@ -22,6 +22,7 @@ public interface Info {
     //asnkdjhaskhfgkjashfghklasjhfgkaslhgfhekjhfk3rgfkjherg
     //dhiuwghfkjwgefkjhwdgkfhgdwkjfhgdwjkfhg
     //asbdfksabfksahgfjkhasgfjkhasgfjkhsa
+    //fkgfjkhdskfsdkgfksdhgfkjsdhfgkjsdgfkdshfg
     int MAJOR=${parsedVersion.majorVersion};
     int MINOR=${parsedVersion.minorVersion};
     int MICRO=${parsedVersion.incrementalVersion};
