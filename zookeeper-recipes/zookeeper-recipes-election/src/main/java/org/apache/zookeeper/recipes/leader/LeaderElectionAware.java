58e2cb7325e353e0556db6b14842a225c5aa0802
Shadan-Harness <156399601+Shadan-Harness@users.noreply.github.com>
2024-01-31 16:26:17 +0530

    Update LeaderElectionAware.java

--- zookeeper-recipes/zookeeper-recipes-election/src/main/java/org/apache/zookeeper/recipes/leader/LeaderElectionAware.java
+++ zookeeper-recipes/zookeeper-recipes-election/src/main/java/org/apache/zookeeper/recipes/leader/LeaderElectionAware.java
@@ -19,6 +19,7 @@ package org.apache.zookeeper.recipes.leader;
 
 import org.apache.zookeeper.recipes.leader.LeaderElectionSupport.EventType;
 //fdfsfsdfg
+int afrfgeferf = 2;
 
 /**
  * An interface to be implemented by clients that want to receive election
